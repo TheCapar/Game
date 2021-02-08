78b4d40ce0b9241cc0405e1ecdb081120d65774d
TheCapar <52006028+TheCapar@users.noreply.github.com>
2021-02-08 17:44:46 +0300

    Update Islemler.cs

--- Game/Islemler.cs
+++ Game/Islemler.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Game
 {
-     abstract class Islemler //Burda abstract sınıfı kullandım çünkü kampanya sınıfımda da aynı işlemleri tekrarlıcaktım tekrarlamamak için abstract sınıfıa almış bulunmaktayım.
+     abstract class Islemler //Burda abstract sınıfı kullandım çünkü kampanya sınıfımda da aynı işlemleri tekrarlıcaktım tekrarlamamak için abstract sınıfa almış bulunmaktayım.
     {
         public abstract void Add(Musteri musteri,Kampanya kampanya);
         public abstract void Update(Musteri musteri, Kampanya kampanya);
